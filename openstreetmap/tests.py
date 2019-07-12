4f47d83296e30a1dd9b3bef97302aee9612d8d03
dagnaldo <naldo.penha@gmail.com>
2019-07-12 18:24:44 -0300

    Criando testes de views

--- openstreetmap/tests.py
+++ openstreetmap/tests.py
@@ -1,13 +1,49 @@
 # -*- coding: utf-8 -*-
 
 from django.test import TestCase
+from django.urls import reverse
 
+from .views import QueryOpenStreetMapReverse, QueryOpenStreetMapSearch
 
-class TestGeocoding(object):
-	"""docstring for TestGeocoding"""
+class TestGeocodingViews(TestCase):
+	"""Teste de views do sistema de geocodificação"""
+
 	def setUp(self):
-		pass
-	def test_url_response(self):
-		pass
+		self.search_url = reverse('osm:osm-search')
+		self.reverse_url = reverse('osm:osm-reverse')
+
+	def test_url_reverse(self):
+		self.assertEqual(self.search_url, '/search/')
+		self.assertEqual(self.reverse_url, '/reverse/')
+
+	def test_url_get_search_response_error(self):
+		response = self.client.get(self.search_url)
+		self.assertEqual(response.status_code, 404)
+		self.assertEqual(response.data['data'], "Parameter endereco is missing")
+
+	def test_url_get_reverse_response_error(self):
+		response = self.client.get(self.reverse_url)
+		self.assertEqual(response.status_code, 404)
+		self.assertEqual(response.data['data'], "Parameter lat is missing")
+
+		response = self.client.get(self.reverse_url, data={"lat": 10})
+		self.assertEqual(response.status_code, 404)
+		self.assertEqual(response.data['data'], "Parameter lon is missing")
+
+	def test_url_post_search_response_error(self):
+		response = self.client.post(self.search_url)
+		self.assertEqual(response.status_code, 404)
+		self.assertEqual(response.data['data'], "Parameter endereco is missing")
+
+	def test_url_post_reverse_response_error(self):
+		response = self.client.post(self.reverse_url)
+		self.assertEqual(response.status_code, 404)
+		self.assertEqual(response.data['data'], "Parameter lat is missing")
+
+		response = self.client.post(self.reverse_url, data={"lat": 10})
+		self.assertEqual(response.status_code, 404)
+		self.assertEqual(response.data['data'], "Parameter lon is missing")
+
+
 	def tearDown(self):
 		pass
